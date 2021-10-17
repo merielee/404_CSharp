ba0d73f9503700a3821137e7cd52dbf669b91bc0
Merielee <1025795@student.sae.edu.au>
2021-10-17 19:46:38 +1000

    Fixing up entry point

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,52 +8,21 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args);
+        public void Setup(string[] args)
 
-        class Player
-        {
-            //Establishing Players
-
-            string Name;
-            int Family;
-            int Friends; 
-
-            void Main(string[] args)
-            {
-                Player spawn = new Player();
-                spawn.Name = "Player 1";
-                spawn.Family = 0;
-                spawn.Friends = 0;
-                Console.WriteLine(spawn.Name);
-                Console.WriteLine(spawn.Family);
-                Console.WriteLine(spawn.Friends);
-
-                Player spawn2 = new Player();
-                spawn2.Name = "Player 2";
-                spawn2.Family = 0;
-                spawn2.Friends = 0;
-                Console.WriteLine(spawn2.Name);
-                Console.WriteLine(spawn2.Family);
-                Console.WriteLine(spawn2.Friends);
-
-                Console.ReadKey();
-
-            }
-        }
 
         //RollDice method
-        void Main(string[] args)
         {
             Random r = new Random();
             int genRand = r.Next(1, 7);
-            System.Console.WriteLine("" + genRand);
+            Console.WriteLine("" + genRand);
 
             Console.ReadKey();
 
             //Add DiceNumber to Lifeareas
             int Lifeareas = 0;
             int NewScore = genRand + Lifeareas;
-            System.Console.WriteLine("" + NewScore);
+            Console.WriteLine("" + NewScore);
 
             //Roll dice until 50 is reach
             Console.WriteLine("Keep living");
