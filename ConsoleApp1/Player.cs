e413ee0845e28ff16ad5b6de73cac16154370435
Merielee <1025795@student.sae.edu.au>
2021-10-18 10:19:37 +1000

    Player Cleaned up

--- ConsoleApp1/Player.cs
+++ ConsoleApp1/Player.cs
@@ -5,35 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 
 public class Player
-
-public void Setup(string[] args)
-
 {
-    //Establishing Players
+    public void Setup(string[] args)
 
-    string Name;
-    int Family;
-    int Friends;
+    {
+        //Establishing Players
 
-	{
-        {
-        Player spawn = new Player();
-        spawn.Name = "Player 1";
-        spawn.Family = 0;
-        spawn.Friends = 0;
-        Console.WriteLine(spawn.Name);
-        Console.WriteLine(spawn.Family);
-        Console.WriteLine(spawn.Friends);
+        string Name;
+        int Family;
+        int Friends;               
+            
+                Player spawn = new Player();
+                spawn.Name = "Player 1";
+                spawn.Family = 0;
+                spawn.Friends = 0;
+                Console.WriteLine(spawn.Name);
+                Console.WriteLine(spawn.Family);
+                Console.WriteLine(spawn.Friends);
 
-        Player spawn2 = new Player();
-        spawn2.Name = "Player 2";
-        spawn2.Family = 0;
-        spawn2.Friends = 0;
-        Console.WriteLine(spawn2.Name);
-        Console.WriteLine(spawn2.Family);
-        Console.WriteLine(spawn2.Friends);
+                Player spawn2 = new Player();
+                spawn2.Name = "Player 2";
+                spawn2.Family = 0;
+                spawn2.Friends = 0;
+                Console.WriteLine(spawn2.Name);
+                Console.WriteLine(spawn2.Family);
+                Console.WriteLine(spawn2.Friends);
 
-        Console.ReadKey();
+                Console.ReadKey();
+                          
 
-        }
 }
