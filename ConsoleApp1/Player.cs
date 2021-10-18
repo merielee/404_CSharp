4f1eaa3de265003d63d997f4d14879f7a1339cd7
Merielee <1025795@student.sae.edu.au>
2021-10-18 15:36:41 +1000

    Player and Dice Update

--- ConsoleApp1/Player.cs
+++ ConsoleApp1/Player.cs
@@ -4,35 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public class Player
+namespace ConsoleApp1
+
 {
-    string Name;
-    int Family;
-    int Friends;
 
-    public void Setup(string[] args)
-
+    public class Player
     {
-        //Establishing Players
+        public string Name;
+        public int Family;
+        public int Friends;
 
+        //Add DiceNumber to Lifeareas
+        public int NewScores = 0;
 
-        Player spawn = new Player();
-        spawn.Name = "Player 1";
-        spawn.Family = 0;
-        spawn.Friends = 0;
-        Console.WriteLine(spawn.Name);
-        Console.WriteLine(spawn.Family);
-        Console.WriteLine(spawn.Friends);
+        
 
-        Player spawn2 = new Player();
-        spawn2.Name = "Player 2";
-        spawn2.Family = 0;
-        spawn2.Friends = 0;
-        Console.WriteLine(spawn2.Name);
-        Console.WriteLine(spawn2.Family);
-        Console.WriteLine(spawn2.Friends);
+        public void Setup(string[] args)
 
-        Console.ReadKey();
+        {
 
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,37 +8,59 @@ namespace ConsoleApp1
 {
     class Program
     {
-        public void Setup(string[] args)
-
-        //RollDice method
+        static int ThrowDice()
         {
             Random r = new Random();
             int genRand = r.Next(1, 7);
             Console.WriteLine("" + genRand);
 
+            return genRand;
+
+        }
+        public static void Main(string[] args)
+
+
+        {
+            //Establishing Players
+
+            Player spawn = new Player();
+            spawn.Name = "Player 1";
+            spawn.Family = 0;
+            spawn.Friends = 0;
+            Console.WriteLine(spawn.Name);
+            Console.WriteLine(spawn.Family);
+            Console.WriteLine(spawn.Friends);
+
+            Player spawn2 = new Player();
+            spawn2.Name = "Player 2";
+            spawn2.Family = 0;
+            spawn2.Friends = 0;
+            Console.WriteLine(spawn2.Name);
+            Console.WriteLine(spawn2.Family);
+            Console.WriteLine(spawn2.Friends);
+
             Console.ReadKey();
 
             //Add DiceNumber to Lifeareas
-            int Lifeareas = 0;
-            int NewScore = genRand + Lifeareas;
-            Console.WriteLine("" + NewScore);
+            spawn.Family = spawn.Family + ThrowDice();
+            Console.WriteLine(spawn.Name);
+            Console.WriteLine(spawn.Family);
+            Console.WriteLine(spawn.Friends);
+
+            spawn2.Family = spawn2.Family + ThrowDice();
+
+            Console.WriteLine(spawn2.Name);
+            Console.WriteLine(spawn2.Family);
+            Console.WriteLine(spawn2.Friends);
+
+            Console.ReadKey();
+
 
             //Roll dice until 50 is reach
             Console.WriteLine("Keep living");
 
-            int TotalScore = NewScore;
-            do
-            {
-                Console.WriteLine(TotalScore);
-                TotalScore++;
-            }
 
-            while (TotalScore < 50);
-
-            System.Console.WriteLine("" + TotalScore);
-
-
-            Console.WriteLine("Now you've live");
+            
 
             Console.Read();
         }
