a9dcb33859db81335f4cca11f22c7cfd926d2a79
Merielee <1025795@student.sae.edu.au>
2021-10-28 09:37:26 +1000

    Class Player creation

--- DiceGame/Class1.cs
+++ DiceGame/Class1.cs
@@ -8,6 +8,11 @@ namespace DiceGame
 {
     class Player
     {
+        static void Main(string[] args)
+
+        Random random = new Random();
+        int familyPoints = random.Next(1, 2);
+        int dice = random.Next(1, 7);
 
     }
 }
