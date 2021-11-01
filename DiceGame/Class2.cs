507a1b475d84bc8a2925b43ee40eaf88a833766b
Merielee <1025795@student.sae.edu.au>
2021-11-01 13:58:40 +1000

    Throw Dice + Random Gen fixed

--- DiceGame/Class2.cs
+++ DiceGame/Class2.cs
@@ -6,8 +6,23 @@ using System.Threading.Tasks;
 
 namespace DiceGame
 {
-    class ThrowDice
-    {            
-        Random rnd = new Random();
-        int rand_num = rnd.Next(1, 7);            
+    class Dice
+    {
+        public void GetNumber()
+
+        {
+            Console.WriteLine("Throw Dice");
+        }
+
+        class RandomGen
+        {
+            public void Rnd()
+            {
+                Random rnd = new Random();
+                int rand_num = rnd.Next(1, 7);
+
+                Console.ReadKey();
+            }
+        }
     }
+}
